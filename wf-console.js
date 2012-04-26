599df8a748438b50b6073b6367273ff9a87dec02
Pedro P. Candel <pedro@joyent.com>
2012-04-26 16:26:41 +0200

    [WORKFLOW-18] Added wf-console: Talk to wf-moray-backend using REPL.

---
+++ wf-console.js
@@ -0,0 +1,69 @@
+var net = require('net'),
+    repl = require('repl'),
+    path = require('path'),
+    fs = require('fs'),
+    util = require('util'),
+    http = require('http'),
+    https = require('https'),
+    wf = require('wf'),
+    config_file = path.resolve(__dirname, 'etc/config.json'),
+    Logger = require('bunyan'),
+    levels = [Logger.TRACE, Logger.DEBUG, Logger.INFO,
+              Logger.WARN, Logger.ERROR, Logger.FATAL],
+    config,
+    log,
+    connections = 0;
+
+fs.readFile(config_file, 'utf8', function (err, data) {
+  if (err) {
+    console.error('Error reading config file:');
+    console.dir(err);
+    process.exit(1);
+  } else {
+    try {
+      config = JSON.parse(data);
+    } catch (e) {
+      console.error('Error parsing config file JSON:');
+      console.dir(e);
+      process.exit(1);
+    }
+
+    if (typeof (config.maxHttpSockets) === 'number') {
+      console.log('Tuning max sockets to %d', config.maxHttpSockets);
+      http.globalAgent.maxSockets = config.maxHttpSockets;
+      https.globalAgent.maxSockets = config.maxHttpSockets;
+    }
+
+    config.logger = {
+      name: 'wf-console',
+      streams: [ {
+        level: 'info',
+        stream: process.stdout
+      }]
+    };
+
+    var Backend = require(config.backend.module),
+        log = new Logger(config.logger),
+        backend = new Backend(config.backend.opts);
+
+    backend.init(function (err) {
+      if (err) {
+        log.error({err: err}, 'Error initializing backend');
+        process.exit(1);
+      }
+      log.info('Backend initialized');
+      net.createServer(function (socket) {
+        connections += 1;
+        var remote = repl.start('wf-console> ', socket);
+        remote.context.backend = backend;
+        remote.context.log = log;
+        remote.context.config = config;
+        remote.context.wf = wf;
+      }).listen('/tmp/node-repl.sock');
+      // nc -U /tmp/node-repl.sock
+    });
+
+
+
+  }
+});
