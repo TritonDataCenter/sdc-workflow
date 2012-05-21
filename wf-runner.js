a96ce900cb5b55a500ad6fe85af59a843cb2b772
Pedro P. Candel <pedro@joyent.com>
2012-05-21 09:21:50 +0200

    WORKFLOW-27: Retry moray connection on init 5 times every 15 seconds (to give the moray zone time to come up).

--- wf-runner.js
+++ wf-runner.js
@@ -13,104 +13,125 @@ var path = require('path'),
     config,
     runner,
     log,
-    agentIntervalId;
+    agentIntervalId,
+    retries = 0,
+    MAX_RETRIES = 5;
 
 
 fs.readFile(config_file, 'utf8', function (err, data) {
-  if (err) {
-    console.error('Error reading config file:');
-    console.dir(err);
-    process.exit(1);
-  } else {
-    try {
-      config = JSON.parse(data);
-    } catch (e) {
-      console.error('Error parsing config file JSON:');
-      console.dir(e);
-      process.exit(1);
-    }
-
-    if (typeof (config.maxHttpSockets) === 'number') {
-      console.log('Tuning max sockets to %d', config.maxHttpSockets);
-      http.globalAgent.maxSockets = config.maxHttpSockets;
-      https.globalAgent.maxSockets = config.maxHttpSockets;
-    }
-
-    config.logger = {
-      streams: [ {
-        level: config.logLevel || 'info',
-        stream: process.stdout
-      }]
-    };
-
-    runner = wf.Runner(config);
-    runner.init(function (err) {
-      if (err) {
-        console.error('Error initializing runner:');
+    if (err) {
+        console.error('Error reading config file:');
         console.dir(err);
         process.exit(1);
-      }
-      runner.run();
-
-      log = runner.log;
-      log.info('Workflow Runner up!');
-
-      // Setup a logger on HTTP Agent queueing
-      agentIntervalId = setInterval(function () {
-        var agent = http.globalAgent;
-        if (agent.requests && agent.requests.length > 0) {
-          log.warn('http.globalAgent queueing, depth=%d',
-                   agent.requests.length);
-          }
-        agent = https.globalAgent;
-        if (agent.requests && agent.requests.length > 0) {
-          log.warn('https.globalAgent queueing, depth=%d',
-                   agent.requests.length);
-          }
-      });
-    });
-
-    process.on('SIGUSR1', function () {
-      function decreaseLevel(logger) {
-        var pos = levels.indexOf(logger._level);
-
-        console.log('Got SIGUSR1. Attempting to decrease log level');
-
-        if (pos === (levels.length + 1)) {
-          console.log('Log level already set to the minimun. Doing nothing');
-        } else {
-          logger.level(levels[pos + 1]);
-          console.log('Log level set to ' + levels[pos + 1]);
+    } else {
+        try {
+            config = JSON.parse(data);
+        } catch (e) {
+            console.error('Error parsing config file JSON:');
+            console.dir(e);
+            process.exit(1);
         }
-      }
-      [runner.log, runner.backend.log,
-        runner.backend.client.client.log].forEach(decreaseLevel);
-    });
 
-    process.on('SIGUSR2', function () {
-      function increaseLevel(logger) {
-        var pos = levels.indexOf(logger._level);
-
-        console.log('Got SIGUSR2. Attempting to increase log level');
-
-        if (pos === 0) {
-          console.log('Log level already set to the maximun. Doing nothing');
-        } else {
-          logger.level(levels[pos - 1]);
-          console.log('Log level set to ' + levels[pos - 1]);
+        if (typeof (config.maxHttpSockets) === 'number') {
+            console.log('Tuning max sockets to %d', config.maxHttpSockets);
+            http.globalAgent.maxSockets = config.maxHttpSockets;
+            https.globalAgent.maxSockets = config.maxHttpSockets;
         }
-      }
-      [runner.log, runner.backend.log,
-        runner.backend.client.pool.log].forEach(increaseLevel);
-    });
 
-    process.on('SIGINT', function () {
-      console.log('Got SIGINT. Waiting for child processes to finish');
-      runner.quit(function () {
-        clearInterval(agentIntervalId);
-        console.log('All child processes finished. Exiting now.');
-        process.exit(0);
-      });
-    });
-  }
+
+
+        config.logger = {
+            streams: [ {
+                level: config.logLevel || 'info',
+                stream: process.stdout
+            }]
+        };
+
+
+        runner = wf.Runner(config);
+        log = runner.log;
+        var init = function (cb) {
+            runner.init(function (err) {
+                if (err) {
+                    retries += 1;
+                    console.error('Error initializing runner:');
+                    console.dir(err);
+                    if (retries >= MAX_RETRIES) {
+                        console.error('Exiting because MAX_RETRIES exceeded');
+                        process.exit(1);
+                    } else {
+                        console.log('Re-queueing runner init in 15 secs');
+                        setTimeout(init, 15000, cb);
+                    }
+                } else {
+                    cb();
+                }
+            });
+        };
+
+        init(function () {
+            runner.run();
+            log.info('Workflow Runner up!');
+        });
+
+        process.on('SIGUSR1', function () {
+            function decreaseLevel(logger) {
+                var pos = levels.indexOf(logger._level);
+
+                console.log('Got SIGUSR1. Attempting to decrease log level');
+
+                if (pos === (levels.length + 1)) {
+                    console.log('Log level already set to the minimun. ' +
+                      'Doing nothing');
+                } else {
+                    logger.level(levels[pos + 1]);
+                    console.log('Log level set to ' + levels[pos + 1]);
+                }
+            }
+            [runner.log, runner.backend.log,
+              runner.backend.client.client.log].forEach(decreaseLevel);
+        });
+
+        process.on('SIGUSR2', function () {
+            function increaseLevel(logger) {
+                var pos = levels.indexOf(logger._level);
+
+                console.log('Got SIGUSR2. Attempting to increase log level');
+
+                if (pos === 0) {
+                    console.log('Log level already set to the maximun. ' +
+                      'Doing nothing');
+                } else {
+                    logger.level(levels[pos - 1]);
+                    console.log('Log level set to ' + levels[pos - 1]);
+                }
+            }
+            [runner.log, runner.backend.log,
+              runner.backend.client.pool.log].forEach(increaseLevel);
+        });
+
+        process.on('SIGINT', function () {
+            console.log('Got SIGINT. Waiting for child processes to finish');
+            runner.quit(function () {
+                clearInterval(agentIntervalId);
+                console.log('All child processes finished. Exiting now.');
+                process.exit(0);
+            });
+        });
+
+
+        // Setup a logger on HTTP Agent queueing
+        agentIntervalId = setInterval(function () {
+            var agent = http.globalAgent;
+            if (agent.requests && agent.requests.length > 0) {
+                log.warn('http.globalAgent queueing, depth=%d',
+                         agent.requests.length);
+              }
+            agent = https.globalAgent;
+            if (agent.requests && agent.requests.length > 0) {
+                log.warn('https.globalAgent queueing, depth=%d',
+                         agent.requests.length);
+              }
+        });
+    }
 });
